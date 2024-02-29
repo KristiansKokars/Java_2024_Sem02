422abf68363aa491eea1a67c4a4a796fac75192c
KristiansKokars <kristians_kokars@outlook.com>
2024-02-29 11:00:03 +0200

    feat: added professor code

--- src/main/java/com/kristianskokars/model/Course.java
+++ src/main/java/com/kristianskokars/model/Course.java
@@ -1,8 +1,73 @@
 package com.kristianskokars.model;
 
+import com.kristianskokars.validator.NameValidator;
+
 public class Course {
+    private static long idCounter = 10000;
+    private static final int CREDIT_POINT_MAX = 20;
+
     private Long id;
     private String title;
     private int creditPoints;
     private Professor professor;
+
+    public Course() {
+        generateId();
+        setTitle("Programmēšana Tīmeklī JAVA");
+        setCreditPoints(4);
+        setProfessor(new Professor());
+    }
+
+    public Course(String title, int creditPoints, Professor professor) {
+        generateId();
+        setTitle(title);
+        setCreditPoints(creditPoints);
+        setProfessor(professor);
+    }
+
+    public void generateId() {
+        id = idCounter;
+        idCounter++;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if (NameValidator.isValidLatvianWord(title)) {
+            this.title = title;
+        } else {
+            this.title = "undefined";
+        }
+    }
+
+    public int getCreditPoints() {
+        return creditPoints;
+    }
+
+    public void setCreditPoints(int creditPoints) {
+        if (creditPoints < 1 || creditPoints > CREDIT_POINT_MAX) {
+            this.creditPoints = 2;
+        } else {
+            this.creditPoints = creditPoints;
+        }
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        if (professor == null) {
+            this.professor = new Professor();
+        } else {
+            this.professor = professor;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return id + ": " + title + "(" + creditPoints + " KP)," + professor;
+    }
 }
--- src/main/java/com/kristianskokars/validator/NameValidator.java
+++ src/main/java/com/kristianskokars/validator/NameValidator.java
@@ -4,4 +4,8 @@ public class NameValidator {
     public static boolean isValidLatvianName(String name) {
         return name.matches("[A-ZŽĒŪĪĻĶĢŠĀČŅ]{1}[a-zžēūīļķģšāčņ]+");
     }
+
+    public static boolean isValidLatvianWord(String word) {
+        return word.matches("[A-ZĒŪĪĻĶĢŠĀČŅa-zēūīļķģšāžčņ ]+");
+    }
 }
